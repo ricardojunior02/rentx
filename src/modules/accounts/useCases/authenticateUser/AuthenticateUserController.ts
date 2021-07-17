b2b01cdc6b92d2fa2df326ac93954d10d66977a4
Ricardo Junior <ricardojunior199502@gmail.com>
2021-07-17 16:03:53 -0300

    controller de autenticação

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { password, email } = request.body;
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+
+    const user = await authenticateUserUseCase.execute({ password, email });
+
+    return response.status(200).json(user);
+  }
+}
+
+export { AuthenticateUserController };
