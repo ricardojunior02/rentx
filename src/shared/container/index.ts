fcbe7478c2313a7069f54ac88f076c55e44b56a9
Ricardo Junior <ricardojunior199502@gmail.com>
2021-07-17 16:45:33 -0300

    instancias globais de classes do app

---
+++ src/shared/container/index.ts
@@ -0,0 +1,49 @@
+import { container } from 'tsyringe';
+
+import '@shared/container/providers';
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
+import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
+import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+import { RentalRepository } from '@modules/rentals/infra/typeorm/repositories/RentalRepository';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+
+container.registerSingleton<ICategoriesRepository>(
+  'CategoriesRepository',
+  CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  'SpecificationsRepository',
+  SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
+);
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>(
+  'CarsImagesRepository',
+  CarsImagesRepository
+);
+
+container.registerSingleton<IRentalsRepository>(
+  'RentalRepository',
+  RentalRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  'UsersTokensRepository',
+  UsersTokensRepository
+);
