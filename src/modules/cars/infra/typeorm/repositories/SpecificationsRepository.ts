61ba8a5e1c78196a028199a5c332343bf5dc5651
Ricardo Junior <ricardojunior199502@gmail.com>
2021-07-17 16:17:19 -0300

    rpositorio de especificações

---
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -0,0 +1,49 @@
+import { getRepository, Repository } from 'typeorm';
+
+import {
+  ISpecificationRepository,
+  ICreateSpecificationDTO,
+} from '@modules/cars/repositories/ISpecificationsRepository';
+
+import { Specification } from '../entities/Specification';
+
+class SpecificationsRepository implements ISpecificationRepository {
+  private repository: Repository<Specification>;
+
+  constructor() {
+    this.repository = getRepository(Specification);
+  }
+
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
+    const specification = this.repository.create({
+      name,
+      description,
+    });
+
+    await this.repository.save(specification);
+
+    return specification;
+  }
+
+  async findByName(name: string): Promise<Specification> {
+    const specification = await this.repository.findOne({ name });
+
+    return specification;
+  }
+
+  async list(): Promise<Specification[]> {
+    const specifications = await this.repository.find();
+    return specifications;
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specification = await this.repository.findByIds(ids);
+
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
