bafd7d3351eb9b9e7313264093df426eab9d02db
Ricardo Junior <ricardojunior199502@gmail.com>
2021-07-17 16:40:05 -0300

    regra de criação de aluguel

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,70 @@
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '@errors/AppError';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+@injectable()
+class CreateRentalUseCase {
+  constructor(
+    @inject('RentalRepository')
+    private rentalsRepository: IRentalsRepository,
+
+    @inject('DayJsDateProvider')
+    private dateProvider: IDateProvider,
+
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository
+  ) {}
+  async execute({
+    car_id,
+    user_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError('Car is unavailable');
+    }
+
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppError('There`s a rental in progress for user!');
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
+
+    if (compare < 24) {
+      throw new AppError('Invalid return time!');
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    await this.carsRepository.updateAvailable(car_id, false);
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
