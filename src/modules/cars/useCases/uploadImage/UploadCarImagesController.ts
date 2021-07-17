af3dc78c6f69321f44770152b2b200ac319867de
Ricardo Junior <ricardojunior199502@gmail.com>
2021-07-17 16:37:29 -0300

    controller de upload de imagens

---
+++ src/modules/cars/useCases/uploadImage/UploadCarImagesController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { UploadCarImageUseCase } from './UploadCarImageUseCase';
+
+interface IFiles {
+  filename: string;
+}
+
+class UploadCarImagesController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const images = request.files as IFiles[];
+    const uploadCarImageUseCase = container.resolve(UploadCarImageUseCase);
+
+    const fileNames = images.map((files) => files.filename);
+
+    await uploadCarImageUseCase.execute({
+      car_id: id,
+      images_name: fileNames,
+    });
+
+    return response.status(201).send();
+  }
+}
+
+export { UploadCarImagesController };
